2bff5362ae0461aa2ef64dadb6a9a914797bc196
root <root@gfernandez832.mylabserver.com>
2015-09-06 03:14:36 +0000

    updating our workstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,6 +1,8 @@
 default["apache"]["sites"]["gfernandez833"] = {"site_title" => "Gfernandez833 websites coming soon", "port" => 80, "domain" => "gfernandez8333.mylabserver.com" }
 default["apache"]["sites"]["gfernandez833b"] = {"site_title" => "Gfernandez833b websites coming soon", "port" => 80, "domain" => "gfernandez833b.mylabserver.com" }
-default["apache"]["sites"]["gfernandez84"] = {"site_title" => "Gfernandez834 websites coming soon", "port" => 80, "domain" => "gfernandez84.mylabserver.com" }
+default["apache"]["sites"]["gfernandez834"] = {"site_title" => "Gfernandez834 websites coming soon", "port" => 80, "domain" => "gfernandez834.mylabserver.com" }
+
+default["author"]["name"] = "gfernandez" 
 
 case node["platform"] 
 when "centos"
